9bdad539cd57d64c55d17464e083f04c8ad7a442
Himanshu <himanshux412@gmail.com>
2026-07-31 16:15:52 +0530

    new

--- BACKEND/EVENTMODULE/event.js
+++ BACKEND/EVENTMODULE/event.js
@@ -1,4 +1,14 @@
-//Event module
-//Event class uses -on() as listener and emint() as a event tr
-const EventEmitter= require('events');
-const event= new EventEmitter();
+// EventEmitter is a class
+// emit("event param"}:trigger/create/fire and on("event emit param")
+ const EventEmitter = require('events');
+ const event = new EventEmitter();
+// event.on("greet", () => {
+//     console.log("This is Event Emitter");
+// })
+event.once("greet",()=>{
+    console.log("call event only once")
+})
+event.emit("greet");
+event.emit("greet");
+event.emit("greet");
+event.emit("greet");
