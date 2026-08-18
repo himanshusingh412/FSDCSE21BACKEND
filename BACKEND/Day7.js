9b6c62c56d11838e576e020ddca95fbc017c4917
Himanshu <himanshux412@gmail.com>
2026-08-18 12:30:05 +0530

    new

--- BACKEND/Day7.js
+++ BACKEND/Day7.js
@@ -1,9 +1,13 @@
-//create your own server using http module
-const http=require('http');
-const server=http.createServer((req,res)=>{
-res.writeHead('Hello World');
-    res.end();
+//create your own server using HTTP module
+
+// const http=require('http');
+import http from "http";
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+//   res.write("Hello World");
+  res.write("<h1>Welcome to my server</h1>");
+  res.end();
 });
-server.listen(8000,()=>{
-    console.log('Server running at http://localhost:8000/');
+server.listen(8000, () => {
+  console.log("server is running on port 8000");
 });
