7d66db6a715b3b9c9304bbd0bdfe21c50ba2e6f0
Himanshu <himanshux412@gmail.com>
2026-09-10 12:21:23 +0530

    new

---
+++ Express_HTML/index.js
@@ -0,0 +1,21 @@
+app.get('/about', (req, res) => {
+    fs.readFile('pages/about.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading about page');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+app.get('/contact', (req, res) => {
+    fs.readFile('pages/contact.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading contact page');
+            return;
+        }
+
+        res.send(data);
+    });
+});
