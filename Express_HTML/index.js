b553b854968559f3d04071057af1c3c0855074d8
Himanshu <himanshux412@gmail.com>
2026-09-11 16:19:51 +0530

    portfolio

--- Express_HTML/index.js
+++ Express_HTML/index.js
@@ -1,7 +1,37 @@
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    fs.readFile('pages/home.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading home page');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
 app.get('/about', (req, res) => {
     fs.readFile('pages/about.html', 'utf-8', (err, data) => {
         if (err) {
             res.status(500).send('Error reading about page');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+app.get('/projects', (req, res) => {
+    fs.readFile('pages/projects.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading projects page');
             return;
         }
 
@@ -18,4 +48,12 @@ app.get('/contact', (req, res) => {
 
         res.send(data);
     });
-});u
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
