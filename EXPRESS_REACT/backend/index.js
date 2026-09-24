c47f5d10c2dca33508a11d8290d6e6aa7b9be9bc
Himanshu <himanshux412@gmail.com>
2026-09-24 12:21:06 +0530

    new

--- EXPRESS_REACT/backend/index.js
+++ EXPRESS_REACT/backend/index.js
@@ -53,6 +53,6 @@ app.delete("/api/products/:id", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(5001, () => {
+  console.log("Server running on http://localhost:5001");
 });
